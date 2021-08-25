76918d8adc7d1d11eb15b465bc87750d49ea9731
Deblina Das <77379919+DeblinaD@users.noreply.github.com>
2021-08-25 12:25:27 +0530

    Create Strings.py

---
+++ Strings.py
@@ -0,0 +1,10 @@
+>>> a= 'Deblina'
+>>> b='Das'
+>>> a+b
+'DeblinaDas' #it is without space
+>>> a+' '+b
+'Deblina Das' #gives space in between
+>>> x= 'Deblina'
+>>> x*3    #without any loop if you want to print any particular string for a certain no. of tomes
+'DeblinaDeblinaDeblina'
+>>> 
