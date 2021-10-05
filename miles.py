b4400312988c8e592c8078483462051d3f25b5f3
Deblina Das <77379919+DeblinaD@users.noreply.github.com>
2021-10-05 09:54:20 +0530

    Create miles.py

---
+++ miles.py
@@ -0,0 +1,4 @@
+#There are 5280 feet in a mile. Write a Python statement that 	calculates and prints the number of feet in 13 milesThere are 5280 feet in a mile. Write a Python statement that calculates and prints the number of feet in 13 miles
+a=int(input())
+a=a*5280
+print(a)
